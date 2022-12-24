d506f06a40b78311ce4c86a4d86a43483229580f
PushyTao <169961689@qq.com>
2022-12-24 20:06:10 +0800

    modified HouJie main.cpp calcel notes

--- Learn/HouJie/main.cpp
+++ Learn/HouJie/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 int main() {
-//    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     auto str = R"code(some description)code"
                "\n Defined in "  __FILE__;
     std::cout << str << std::endl;
